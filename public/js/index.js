7029161146593a55289f1cda274857615445645d
Shivam Agarwal <shivam.agarwal18@outlook.com>
2018-05-26 17:21:13 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -39,12 +39,12 @@ socket.on('newLocationMessage', function(message){
 
 $('#message-form').on('submit',function(e){
   e.preventDefault();
-
+  var messageTextBox = $('[name=message]');
   socket.emit('createMessage',{
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextBox.val()
   },function(){
-
+    messageTextBox.val('');
   });
 });
 
@@ -53,13 +53,15 @@ locationButton.on('click', function(e){
   if (!navigator.geolocation){
     return alert('Geolocation not supported by browser !')
   }
-
+  locationButton.attr('disabled','disabled').text('Sending Location');
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage',{
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function(){
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location !')
   });
 });
