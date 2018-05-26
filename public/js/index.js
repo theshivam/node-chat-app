868f93b2fae576a49b25d3f3d17a1716de03d7bc
Shivam Agarwal <shivam.agarwal18@outlook.com>
2018-05-27 01:37:39 +0530

    Scroll to bottom if the scrollbar is near to the last message

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 console.log('Inside index.js');
+
+function scrollToBottom(){
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var prevMessageHeight = newMessage.prev().innerHeight();
+  if (clientHeight + scrollTop + newMessageHeight + prevMessageHeight >= scrollHeight){
+     messages.scrollTop(scrollHeight);
+  }
+
+}
+
 socket.on('connect', function() {
   console.log('Connected to the server !');
 
@@ -19,6 +34,7 @@ socket.on('newMessage',function(message){
     createdAt:formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 socket.on('disconnect',function() {
   console.log('Disconnected from the server !');
@@ -47,6 +63,7 @@ socket.on('newLocationMessage', function(message){
   // a.attr('href',message.url);
   // li.append(a);
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on('submit',function(e){
